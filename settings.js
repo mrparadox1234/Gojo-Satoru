781da007de79d9abf6689bccd63aa761a84afdd5
mrparadox1234 <111228327+mrparadox1234@users.noreply.github.com>
2022-08-14 23:23:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94771159121']
+global.premium = ['94771159121']
+global.ownernomer = '94771159121'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
